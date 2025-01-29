498c893a02b1052f5afe3c0887557758f3b5f8ac
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2025-01-30 01:41:19 +0200

    CamperFeatures and FeaturesIcons

--- src/components/CamperFeatures/CamperFeatures.jsx
+++ src/components/CamperFeatures/CamperFeatures.jsx
@@ -1,5 +1,35 @@
+import FeaturesIcons from "./FeaturesIcons";
+
 const CamperFeatures = () => {
-  return <div></div>;
+  return (
+    <div>
+      <FeaturesIcons />
+
+      <h2 className="">Vehicle details</h2>
+      <div className="my-4 h-px w-full bg-gray-300"></div>
+
+      <ul className="">
+        <li className="flex justify-between">
+          <span className="">Form:</span> <span>Panel truck</span>
+        </li>
+        <li className="flex justify-between">
+          <span className="">Length:</span> <span>5.4 m</span>
+        </li>
+        <li className="flex justify-between">
+          <span className="">Width:</span> <span>2.01 m</span>
+        </li>
+        <li className="flex justify-between">
+          <span className="">Height:</span> <span>2.05 m</span>
+        </li>
+        <li className="flex justify-between">
+          <span className="">Tank:</span> <span>132 l</span>
+        </li>
+        <li className="flex justify-between">
+          <span className="">Consumption:</span> <span>12.4l/100km</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default CamperFeatures;
--- src/components/CamperFeatures/FeaturesIcons.jsx
+++ src/components/CamperFeatures/FeaturesIcons.jsx
@@ -1,7 +1,30 @@
 import Icon from "../../ui/Icon";
 
 const FeaturesIcons = () => {
-  return <div></div>;
+  return (
+    <div>
+      <span className="">
+        <Icon id="diagram" w={20} h={20} className="fill-black" />
+        Automatic
+      </span>
+      <span className="">
+        <Icon id="wind" w={20} h={20} className="fill-black" />
+        AC
+      </span>
+      <span className="">
+        <Icon id="fuel-pump" w={20} h={20} className="fill-black" />
+        Petrol
+      </span>
+      <span className="">
+        <Icon id="cup-hot" w={20} h={20} className="fill-black" />
+        Kitchen
+      </span>
+      <span className="">
+        <Icon id="ui-radios" w={20} h={20} className="fill-black" />
+        Radio
+      </span>
+    </div>
+  );
 };
 
 export default FeaturesIcons;
